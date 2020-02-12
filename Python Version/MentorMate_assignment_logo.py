2bc721abc3c7a6aabfacbf67bb53c0d11ace4879
iEatTomatoesJack <jackangel.dev@gmail.com>
2020-02-12 17:02:44 +0200

    Python Version\MentorMate_assignment_logo.py

--- Python Version/MentorMate_assignment_logo.py
+++ Python Version/MentorMate_assignment_logo.py
@@ -1,33 +1,42 @@
 """This is the solution for the MentorMate assignment using python"""
 class DoubleMLetter:
-    def logo_line(self,i):  # returns the symbols for one line, for one M
-        result = "" # variable to hold the created line
-        if i < self.m_width / 2:  # for the upper half of the logo this pattern is used
-            result = "-" * self.logo_side_dash_num + "*" * self.logo_star_num\
-                     + "-" * self.logo_upper_dash_num + "*" * self.logo_star_num\
-                     + "-" * self.logo_side_dash_num
-            self.logo_upper_dash_num -= 2
-            self.logo_star_num += 2
-        else: # for the lower half of the logo this pattern is used
-            self.logo_star_num -= 2
-            result = "-" * self.logo_side_dash_num + "*" * self.m_width  \
-                     +"-" * self.logo_lower_dash_num + "*" * self.logo_star_num\
-                     + "-" * self.logo_lower_dash_num + "*" * self.m_width \
-                     + "-" * self.logo_side_dash_num
-            self.logo_lower_dash_num += 2
+    def logo_line(self,current_line): 
+        result = "" 
+        if current_line < self.m_width / 2: 
+            result = (
+                     "-" * self.logoSideDashNum
+                     + "*" * self.logoStarNum\
+                     + "-" * self.logoUpperDashNum
+                     + "*" * self.logoStarNum\
+                     + "-" * self.logoSideDashNum
+                     )
+            self.logoUpperDashNum -= 2
+            self.logoStarNum += 2
+        else: 
+            self.logoStarNum -= 2
+            result = (
+                     "-" * self.logoSideDashNum
+                     + "*" * self.m_width  \
+                     +"-" * self.logoBottomHalfDashNum
+                     + "*" * self.logoStarNum\
+                     + "-" * self.logoBottomHalfDashNum
+                     + "*" * self.m_width \
+                     + "-" * self.logoSideDashNum
+                     )
+            self.logoBottomHalfDashNum += 2
 
-        self.logo_side_dash_num -= 1
-        return result * 2  # returns a string which contains the result 2 times, so that we have the needed parts for 2 "M" letters
+        self.logoSideDashNum -= 1
+        return result * 2  
 
-    def print_logo(self,width): # prints the MM logo by given width for the M  character
+    def print_logo(self,width): 
         self.m_width = width
-        self.logo_star_num = width
-        self.logo_side_dash_num = width
-        self.logo_upper_dash_num = width
-        self.logo_lower_dash_num = 1
-        for i in range(width + 1):
-           print(self.logo_line(i))
+        self.logoStarNum = width
+        self.logoSideDashNum = width
+        self.logoUpperDashNum = width
+        self.logoBottomHalfDashNum = 1
+        for current_line in range(width + 1):
+           print(self.logo_line(current_line))
 
-m_width = int(input()) # get the m_widthber from the console
-mm = DoubleMLetter() # create a MentorMate class
-mm.print_logo(m_width) # print the MentorMate logo
+widthInput = int(input("Write the width of the double M: "))
+doubleM = DoubleMLetter() 
+doubleM.print_logo(widthInput) 
